c7fc921b13306fb1bbf9144164c607cfbf488f63
Bruno <bruno.adamm@gmail.com>
2020-10-11 16:37:25 -0300

    part 1

---
+++ numbertrivia/lib/features/numberTrivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:numbertrivia/core/failures.dart';
+import 'package:numbertrivia/features/numberTrivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
